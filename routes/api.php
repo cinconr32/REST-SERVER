9d82e20ddc6d99b2bab9e2d9c7d2d901f4bdd505
cinconr32 <akunviu32@gmail.com>
2022-11-24 01:53:44 +0700

    creating routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\TagihanController;
+use App\Http\Controllers\API\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'test', 'namespace' => 'App\Http\Controllers\API'], function() {
+    Route::apiResource('customer', CustomerController::class);
+    Route::apiResource('tagihan', TagihanController::class);
 });
